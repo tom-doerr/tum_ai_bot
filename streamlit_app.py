6661547ed1e4c880dc51be1d4b7f3a58ad816899
Tom Dörr <tom.doerr@tum.de>
2022-06-13 18:36:16 +0200

    Update file

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,9 @@ import openai
 import configparser
 
 
-CONFIG_DIR = os.getenv('XDG_CONFIG_HOME', os.path.expanduser('~/.config'))
-API_KEYS_LOCATION = os.path.join(CONFIG_DIR, 'openaiapirc')
+# CONFIG_DIR = os.getenv('XDG_CONFIG_HOME', os.path.expanduser('~/.config'))
+# API_KEYS_LOCATION = os.path.join(CONFIG_DIR, 'openaiapirc')
+API_KEYS_LOCATION = os.path.join(CONFIG_DIR, '.streamlit/secrets.toml')
 
 def create_template_ini_file():
     """
