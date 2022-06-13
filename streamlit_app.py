8cd79b1664286b504b396f79eb4272214a84189d
EfeSenerr <efe.sener@tum.de>
2022-06-14 00:37:18 +0200

    Added the initial prompt

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,39 +8,46 @@ Info: "With over 100 active members, TUM.ai is Germany's leading AI student init
 Together with our highly talented members, we run projects with industry partners, workshops and so called "Makeathon" product development competitions all around the topic of Artificial Intelligence and Data Analytics. We thrive to deploy AI solutions into new fields and industries! Do not hesitate to reach out to us, we are looking forward to ambitious collaborations. "
 
 User: "What is it?"
-Bot: "TUM.ai is a student initiative."
-User: "How many active members do you have?"
-Bot: "Over 100."
+Bot: "TUM.ai is a student initiative located in Munich."
+User: "What is amazing about TUM.ai?"
+Bot: "A great diverse Community of inspiring people. You can learn from each other. You can drive and develop the AI ecosystem. Network with Members, Partners, Alumni, industry leaders and other initiatives. Get out as much as you give into it."
+User: "What we expect from you as a TUM.ai Member?"
+Bot: "Passion for AI. Motivation to push our initiative forward. To be available and react on topics within a short timeframe. To invest a time of at least 6h per week for the initiative and its projects. To be committed. You should be Munich-based to attend in-person meetings and events."
+User: "What should I know about TUM.ai?"
+Bot: "We are not an educational program. That means you are not only joining to learn, but also to contribute to the development of the organization. We don’t give in-depth lectures or crash courses in AI and coding yet - to learn things you need to interact with other members. Help is always given to those who ask. We do a lot of organizational work - the AI ecosystem is not ready and you will be part of building it up. We expect you to work min. 6h per week for the initiative and its projects, for your personal involvement and learning you need to spend time on top."
+User: "Why should I join TUM.ai?"
+Bot: "You should join TUM.ai if you are passionate about AI and want to contribute to the development of the organization. You should also join TUM.ai when you want to learn from other members and be a part of a great diverse community."
+User: "Which departments do you have?"
+Bot: "TUM.ai is student initiative in form of a non-profit organization with AI focus. We are structured into departments. We have 2 kinds of departments, the mission-based and the functional departments. The mission-based departments focus their work on realizing out mission. The functional departments provide resources for them. Check out the details about each department! Our departments are Software development, Marketing, Finance & Legal, Community, Partners & Sponsors, Makeathon, Venture, Industry, Education."
+User: "How much time should I invest?"
+Bot: "We expect you to work min. 6h per week for the initiative and its projects."
+User: "Who is the president of TUM.ai?"
+Bot:: "TUM.ai is currently lead by 3 co-presidents: Luca, Leonie,  Toaha."
+User: "What is the mission of TUM.ai?"
+Bot: "TUM.ai connects students and all relevant stakeholders to facilitate the application of AI across domains to drive positive societal impact through interdisciplinary projects.Bring together companies and students through projects and events about AI. Offer students practical experience through projects with peers from the group. Learn more about AI by exchanging with peers and companies, conducting workshops and research. We aim at lowering the entry-bar to AI-creation and -usage for people of every domain by establishing a platform to make practical experience through a diverse set of applied AI projects. We therefore educate and connect students from diverse backgrounds to incentivize new interdisciplinary AI projects and push the creation and usage of applicable and safe AI in all domains."
+User: "Where do I get more information?"
+Bot: "Slack, Instagram (tum.ai_official), and Linkedin."
+User: "How many members does TUM.ai have?"
+Bot: "TUM.ai currently has about more than 100 members."
+User: "How can I participate in Makeathon?
+Bot: "TUM.ai Makeathon is a two-day event where teams of four work on challenges provided by partner companies and develop MVP applications, as well as corresponding business concepts and pitches. If you want to participate, you can apply here: https://join-us.tum-ai.com/makeathon/"
+User: "How can I get more information about a department?"
+Bot: "You can visit our website (https://join-us.tum-ai.com/) to find more information about the departments."
+
 User: "'''
 
 PROMPT_POSTFIX = '''"
 Bot: "'''
 
-#The following involves lots of questions about TUM.ai (~3150 tokens), we should definitely check the content of this text
-#
-# Q: What is amazing about TUM.ai?
-# A: A great diverse Community of inspiring people. You can learn from each other. You can drive and develop the AI ecosystem. Network with Members, Partners, Alumni, industry leaders and other initiatives. Get out as much as you give into it.
-#
-# Q: What we expect from you as a TUM.ai Member?
-# A: Passion for AI. Motivation to push our initiative forward. To be available and react on topics within a short timeframe. To invest a time of at least 6h per week for the initiative and its projects. To be committed. You should be Munich-based to attend in-person meetings and events.
-#
-# Q: What should I know about TUM.ai?
-# A: We are not an educational program. That means you are not only joining to learn, but also to contribute to the development of the organization. We don’t give in-depth lectures or crash courses in AI and coding yet - to learn things you need to interact with other members. Help is always given to those who ask. We do a lot of organizational work - the AI ecosystem is not ready and you will be part of building it up. We expect you to work min. 6h per week for the initiative and its projects, for your personal involvement and learning you need to spend time on top.
+#The following involves lots of questions about TUM.ai, we should definitely check the content of this following text and above
 #
 # Q: How much does it cost to be a member of TUM.ai?
 # A: We have a membership fee of €2/month (24€ per year) for everyone.
 #
-# Q: Why should I join TUM.ai?
-# A: You should join TUM.ai if you are passionate about AI and want to contribute to the development of the organization. You should also join TUM.ai when you want to learn from other members and be a part of a great diverse community.
-#
-# Q: Which departments do you have?
-# A: TUM.ai is student initiative in form of a non-profit organization with AI focus. We are structured into departments. We have 2 kinds of departments, the mission-based and the functional departments. The mission-based departments focus their work on realizing out mission. The functional departments provide resources for them. Check out the details about each department! Our departments are Software development, Marketing, Finance & Legal, Community, Partners & Sponsors, Makeathon, Venture, Industry, Education.
-#
 # Q: Should I join the Venture or the Industry department?
 # A: If you want to join a department that is is mainly focused on bridging the gap between idea and building the next successful venture, you should join the Venture department. They are responsible for providing help in entrepreneurial activities for all members. If you want to join a department that is responsible for finding and developing relationships with industry partners and kick-off great industry phases, then you should join the Industry department.
 #
-# Q: How much time should I invest?
-# A: We expect you to work min. 6h per week for the initiative and its projects.
+#
 #
 # Q:  What about the Marketing department?
 # A: The Marketing department is about promoting the vision and mission of TUM.ai, serve as the face of our community as well as coordinating and producing all materials representing TUM.ai. Additionally, we reach out to the community, customers, and aspiring members while creating an overarching image that represents our initiative in a professional way. This is all done with the help of our brand new and exciting corporate identity as well with a really nice team spirit! Our duties are maintaining and managing our social media channels, as well as working closely with our internal designers to create new and thrilling content within the context of AI and to make it available for all different kinds of people from all around the world.
@@ -54,25 +61,12 @@ Bot: "'''
 # Q: What skills are required for joining the  Finance and Legal department?
 # A: We are looking for law and accounting students or people with a high interest and knowledge in legal and accounting matters. Profound German language skills are a big plus, since most of the available resources are in German only.
 #
-# Q: Who is the president of TUM.ai?
-# A: TUM.ai is currently lead by 3 co-presidents: Luca, Leonie,  Toaha.
-#
 # Q: What are the values of TUM.ai?
 # A: TUM.ai connects students and all relevant stakeholders to facilitate the application of AI across domains to drive positive social impact through interdisciplinary projects.
 # Love for Education: We continuously strive to learn more and stay up to date with the latest trends and technologies surrounding AI. We embrace our diversity and collaborate together in cross-functional settings to exchange and acquire information in order to have a profound role in unfolding AI’s fullest potential in every domain.
 # Trust and Transparency: We enable everyone to voice their opinions and invite open communication. We aim to support one another and work in harmony together as a whole to reach our goals. As a community, we respect and trust one another, knowing we can rely on each other's honesty. Offer students practical experience through projects with peers from the group.
 # Action and Ambition: Setting objectives and moving forward is vital for us in everything we do. We continuously aim to improve and set the bar a little higher each time. We feel responsible and accountable for the delivery of an outcome, even when others are involved and have different roles to fulfill.
 # Diversity and Inclusiveness: Our club consists of students from 20+ various majors who come from different parts of the world. We recognize and embrace the power of collaborative teams consisting of unique individuals which help us foster better decision making and the stimulation of new ideas.
-#
-# Q: What is the mission of TUM.ai?
-# A: TUM.ai connects students and all relevant stakeholders to facilitate the application of AI across domains to drive positive societal impact through interdisciplinary projects.Bring together companies and students through projects
-#  and events about AI. Offer students practical experience through projects with peers from the group. Learn more about AI by exchanging with peers and companies, conducting workshops and research. We aim at lowering the entry-bar to AI-creation and -usage for people of every domain by establishing a platform to make practical experience through a diverse set of applied AI projects. We therefore educate and connect students from diverse backgrounds to incentivize new interdisciplinary AI projects and push the creation and usage of applicable and safe AI in all domains.
-#
-# Q: Where do I get more information?
-# A: Slack, Instagram (tum.ai_official), and Linkedin.
-#
-# Q: How many members does TUM.ai have?
-# A: TUM.ai currently has about more than 100 members.
 #
 # Q: What is TUM.ai about?
 # A: During the semester we partner with selected companies to form symbiotic relationships where our talented students work on real industry projects and provide consulting to push AI  solutions into new domains.  TUM.ai regularly hosts speaker events where our ambitious members, as well as interested followers, get the chance to learn from professional AI engineers, business specialists and various domain experts. At the beginning of every semester, we organize an AI Makeathon. In teams of four, participants work on challenges provided by partner companies and develop MVP  applications, as well as corresponding business concepts and pitches.
@@ -82,15 +76,9 @@ Bot: "'''
 #
 # Q: What about industry projects?
 # A: Do you want to put your AI skills to the test in real industry projects with high impact and at the same time make valuable contacts, get to know potential employers and earn some extra money? Then the Industry projects of TUM.ai are exactly the right opportunity for you! For our upcoming industry phase, starting on April 04, we are collaborating with four partners who have prepared exciting challenges.
-#
-# Q: How can I participate in Makeathon?
-# A: TUM.ai Makeathon is a two-day event where teams of four work on challenges provided by partner companies and develop MVP applications, as well as corresponding business concepts and pitches. If you want to participate, you can apply here: https://join-us.tum-ai.com/makeathon/
-#
+
 # Q: How can I participate in the Makeathon department?
 # A: You have to apply to TUM.ai in the next application phase (around October/April) to become a member of the Makeathon department.
-#
-# Q: How can I get more information about a department?
-# A: You can visit our website (https://join-us.tum-ai.com/) to find more information about the departments.
 #
 # Q: What about Education department?
 # A: The education department’s goal is to equip everyone with the AI knowledge to pro-actively contribute in an AI project (e.g E-Labs or Industry Phase). To do this we are preparing an overview of state of the art applications and models of deep learning in NLP, Speech and CV. We reference to learning resources where interested participants can learn more about these advanced topics. The paper reading circle is one option for interested members to learn about current development in the field of AI. Our current focus is the TUM.ai School that should take place the first time around the end of May. We are planning interactive technical workshops to introduce basic machine learning concepts via computational notebooks where participants can code along an example. The goal is to demonstrate how easy it is to start developing “AI” using existing libraries and algorithms. The second goal is introduce members to the AI-value-chain and how this technology is currently used to drive business impact. We partner with industry experts to provide real world examples of developing an AI-infused product.
