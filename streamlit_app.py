83b5db8ae930bda7792723e54dba3d5462b30abd
Tom Dörr <tom.doerr@tum.de>
2022-06-13 18:42:20 +0200

    Update file

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,38 +25,16 @@ import configparser
 
 # CONFIG_DIR = os.getenv('XDG_CONFIG_HOME', os.path.expanduser('~/.config'))
 # API_KEYS_LOCATION = os.path.join(CONFIG_DIR, 'openaiapirc')
-API_KEYS_LOCATION = os.path.join('', '.streamlit/secrets.toml')
 
-def create_template_ini_file():
-    """
-    If the ini file does not exist create it and add the organization_id and
-    secret_key
-    """
-    if not os.path.isfile(API_KEYS_LOCATION):
-        with open(API_KEYS_LOCATION, 'w') as f:
-            f.write('[openai]\n')
-            f.write('organization_id=\n')
-            f.write('secret_key=\n')
-
-        print('OpenAI API config file created at {}'.format(API_KEYS_LOCATION))
-        print('Please edit it and add your organization ID and secret key')
-        print('If you do not yet have an organization ID and secret key, you\n'
-               'need to register for OpenAI Codex: \n'
-                'https://openai.com/blog/openai-codex/')
-        sys.exit(1)
 
 
 def initialize_openai_api():
     """
     Initialize the OpenAI API
     """
-    # Check if file at API_KEYS_LOCATION exists
-    create_template_ini_file()
-    config = configparser.ConfigParser()
-    config.read(API_KEYS_LOCATION)
 
-    openai.organization_id = config['openai']['organization_id'].strip('"').strip("'")
-    openai.api_key = config['openai']['secret_key'].strip('"').strip("'")
+    openai.organization_id = st.secrets['organization_id']
+    openai.api_key = st.secrets['secret_key']
 
 
 
